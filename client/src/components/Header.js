033cd001157661a9cb0b720ffaf964c19f2366ec
Артём <33321841+tema1411@users.noreply.github.com>
2020-01-11 14:58:46 +0200

    add Header layout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <header>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="/" className="left brand-logo">
+              Emaily
+            </a>
+            <ul className="right">
+              <li>
+                <a href="#">Login With Google</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
   }
 }
 
