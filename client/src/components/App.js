55aa98501f0db75f0407bde1889bbad2436e6b40
Artem Ilchenko <artem@oxgaming.com>
2020-01-15 15:04:06 +0200

    test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
+console.log('test');
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
