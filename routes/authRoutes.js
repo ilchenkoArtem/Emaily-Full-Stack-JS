5ff07a4bb797c19867d7e0b126bf3a2fb4fceab7
artem <tema1411@yahoo.com>
2020-01-15 17:28:49 +0200

    test deploy heroku

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,31 +1,35 @@
 const passport = require("passport");
 
 module.exports = app => {
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", { scope: ["profile", "email", "phone"] })
-  );
+    app.get(
+        "/auth/google",
+        passport.authenticate("google", {scope: ["profile", "email", "phone"]})
+    );
 
-  app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/login" }),
-    (req, res) => {
-      // Successful authentication, redirect home.
-      res.redirect("/surveys");
-    }
-  );
+    app.get(
+        "/auth/google/callback",
+        passport.authenticate("google", {failureRedirect: "/login"}),
+        (req, res) => {
+            // Successful authentication, redirect home.
+            res.redirect("/surveys");
+        }
+    );
 
-  app.get("/test", (req, res) => {
-    res.send("Hello");
-  });
+    api.get("/", (req, res) => {
+        res.sendFile("../client/build/index.html")
+    });
 
-  app.get("/api/logout", (req, res) => {
-    req.logout();
-    //res.send(req.user);
-    res.redirect("/");
-  });
+    app.get("/test", (req, res) => {
+        res.send("Hello");
+    });
 
-  app.get("/api/current_user", (req, res) => {
-    req.user ? res.send(req.user) : res.send("");
-  });
+    app.get("/api/logout", (req, res) => {
+        req.logout();
+        //res.send(req.user);
+        res.redirect("/");
+    });
+
+    app.get("/api/current_user", (req, res) => {
+        req.user ? res.send(req.user) : res.send("");
+    });
 };
