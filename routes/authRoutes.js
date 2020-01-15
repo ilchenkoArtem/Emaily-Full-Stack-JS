676ea7316115b34d113ad7b59a931fed2202d7cb
Артём <33321841+tema1411@users.noreply.github.com>
2020-01-15 22:50:09 +0200

    Change format and remove error route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,35 +1,31 @@
 const passport = require("passport");
 
 module.exports = app => {
-    app.get(
-        "/auth/google",
-        passport.authenticate("google", {scope: ["profile", "email", "phone"]})
-    );
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email", "phone"] })
+  );
 
-    app.get(
-        "/auth/google/callback",
-        passport.authenticate("google", {failureRedirect: "/login"}),
-        (req, res) => {
-            // Successful authentication, redirect home.
-            res.redirect("/surveys");
-        }
-    );
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => {
+      // Successful authentication, redirect home.
+      res.redirect("/surveys");
+    }
+  );
 
-    api.get("/", (req, res) => {
-        res.sendFile("../client/build/index.html")
-    });
+  app.get("/test", (req, res) => {
+    res.send("Hello");
+  });
 
-    app.get("/test", (req, res) => {
-        res.send("Hello");
-    });
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    //res.send(req.user);
+    res.redirect("/");
+  });
 
-    app.get("/api/logout", (req, res) => {
-        req.logout();
-        //res.send(req.user);
-        res.redirect("/");
-    });
-
-    app.get("/api/current_user", (req, res) => {
-        req.user ? res.send(req.user) : res.send("");
-    });
+  app.get("/api/current_user", (req, res) => {
+    req.user ? res.send(req.user) : res.send("");
+  });
 };
