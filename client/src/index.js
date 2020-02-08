6d3171608a18865f1d50bdce7811df6c37ee1542
Артём <33321841+tema1411@users.noreply.github.com>
2020-02-08 23:48:45 +0200

    change template for mail

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import reduxThunk from "redux-thunk";
+
 import App from "./components/App";
 import reducers from "./reducers";
+
+//Development helper
+import axios from "axios";
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
