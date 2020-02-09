2f64e455d07d78f35596c5b7ecedcdb611228196
Артём <33321841+tema1411@users.noreply.github.com>
2020-02-09 23:22:35 +0200

    add form for create survey

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,12 +1,50 @@
 //SurveyForm Shows a from for a user to add input
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
 
-class SurveyForm extends Component {
-  render() {
-    return <div>SurveyForm!</div>;
+const FIELDS = [
+  {
+    label: "Survey title",
+    name: "title"
+  },
+  {
+    label: "Subject Line",
+    name: "subject"
+  },
+  {
+    label: "Email Body",
+    name: "body"
+  },
+  {
+    label: "Recipient List",
+    name: "emails"
   }
-}
+];
+
+const SurveyForm = ({ handleSubmit }) => {
+  const renderFields = () => {
+    return FIELDS.map(({ label, name }) => (
+      <Field
+        key={name}
+        label={label}
+        type="text"
+        name={name}
+        component={SurveyField}
+      />
+    ));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(values => console.log(values))}>
+        <ul>{renderFields()}</ul>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default reduxForm({
   form: "surveyForm"
